4c69da169c90e815a99701950ab82fa229c2cc10
Leonardo Baldin <leobaldin.2000@gmail.com>
2017-05-03 00:07:08 +0200

    Finito CustomTextPanel, aggiornato TODO

--- TODO.md
+++ TODO.md
@@ -2,6 +2,6 @@
 - **Mettere a posto** setContentAreaFilled non funziona ***SOLO SU MAC*** per qualche strana
     ragione - trovare il modo di non far creare il JButton brutto e poco stiloso.
     
-- Finire di sistemare StoryTextPanel
+- Finire di sistemare StoryTextPanel / Vedere se renderizza correttamente su Windows
 
 - Aggiustare nextIndex1 e nextIndex2 di tutte le Choice nel json
--- src/UI/Frames/CustomTextPanel.java
+++ src/UI/Frames/CustomTextPanel.java
@@ -13,9 +13,10 @@ import java.io.IOException;
 /**
  * Created by Leonardo Baldin on 02/05/17.
  */
+
 public class CustomTextPanel extends JPanel {
 
-    private static final Color DEFAULT_COLOR = new Color(0, 0, 0, 120);
+    private static final Color OVERLAY_COLOR = new Color(0, 0, 0, 120);
 
     private JLabel testoh = new JLabel() {
         @Override
@@ -27,6 +28,7 @@ public class CustomTextPanel extends JPanel {
     private JFrame root;
 
     private static final String FONT_FILE = "Resources/Cutrims.otf";
+    private Color FONT_COLOR = Color.WHITE;
     private float fontSize = 36f;
     private Font font;
 
@@ -45,12 +47,13 @@ public class CustomTextPanel extends JPanel {
         }
         this.setBorder(new EmptyBorder(new Insets(this.getHeight()/6, this.getWidth()/7, 0, this.getWidth()/7)));
 
-        setFocusable(false);
         setMinimumSize(new Dimension(larghezza, altezza));
     }
 
     private void _initTesto(String testo) {
         testoh.setText(testo);
+        testoh.setForeground(FONT_COLOR);
+        testoh.setOpaque(false);
         add(testoh);
 
         try {
@@ -78,25 +81,26 @@ public class CustomTextPanel extends JPanel {
         testoh.setFont(font);
     }
 
-    public void setCustomText(String newText) {
+    public void setNewText(String newText) {
         testoh.setText(newText);
     }
 
     public void setTextColor(Color newColor) {
+        FONT_COLOR = newColor;
         testoh.setForeground(newColor);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        Polygon fill = new Polygon();
-        fill.addPoint(this.getX(), this.getY());
-        fill.addPoint(this.getX(), this.getWidth());
-        fill.addPoint(this.getWidth(), this.getHeight());
-        fill.addPoint(this.getHeight(), this.getY());
-
-        g.setColor(DEFAULT_COLOR);
-        g.drawImage(ImageResizer.resizeImage(BACKGROUND_IMAGE, root.getWidth(), root.getHeight(), true), 0, 0, null);
-        g.fillPolygon(fill);
         super.paintComponent(g);
+        Polygon fill = new Polygon();
+        fill.addPoint(getX(), getY());
+        fill.addPoint(this.getWidth(), getY());
+        fill.addPoint(this.getWidth(), this.getHeight());
+        fill.addPoint(getX(), this.getHeight());
+
+        g.drawImage(ImageResizer.resizeImage(BACKGROUND_IMAGE, root.getWidth(), root.getHeight(), true), 0, 0, null);
+        g.setColor(OVERLAY_COLOR);
+        g.fillPolygon(fill);
     }
 }
